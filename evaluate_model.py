b78ee79e18f2b8b7e3a1e0e8998e66fd65bc2841
KORALLLL <kirbor2014@ya.ru>
2025-07-19 17:34:26 +0000

    ADD: time computation during models inferenece

--- evaluate_model.py
+++ evaluate_model.py
@@ -1,8 +1,17 @@
 import torch, pathlib, yaml, argparse
 from tqdm import tqdm
 from loguru import logger
+import time
+import numpy as np
+from scipy.stats import t
 
+def mean_ci_halfwidth(values, alpha=0.05):
 
+    x = np.asarray(values, dtype=float)
+    mean = x.mean()
+    se   = x.std(ddof=1) / np.sqrt(len(x))        
+    h    = t.ppf(1 - alpha/2, df=len(x) - 1) * se 
+    return mean, h
 
 class VideoDS(torch.utils.data.Dataset):
     def __init__(self, root: pathlib.Path, metapath):
@@ -64,19 +73,28 @@ if __name__ == "__main__":
     if not CONTINUE_FROM:
         with open(OUTPUT_RESULTS, "w") as f_out:
             f_out.write(f"PROMPT:\n\n\n\n{PROMPT}\n\n\n\n")
-
+    results = []
     with torch.inference_mode():
         for idx in tqdm(range(len(ds)), total=len(ds)):
-            # try:
+            try:
+                start = time.time()
                 sample = ds[idx]
                 if CONTINUE_FROM:
                     if idx<CONTINUE_FROM: continue
                 inputs = backend.encode_query(sample["path"], PROMPT, fps=FPS, num_frames=NUM_FRAMES)
                 reply  = backend.generate(inputs, max_new_tokens=MAX_NEW_TOKENS)
+                end = time.time()
 
                 line = f"{sample['path']}\t{reply}\n"
                 with open(OUTPUT_RESULTS, "a") as f_out:
                     f_out.write(line)
                 logger.info(line.strip())
-            # except:
-            #     logger.error(sample['path'])
+                logger.info(str(end-start))
+                results.append(end-start)
+
+            except:
+                logger.error(sample['path'])
+
+
+    m, hw = mean_ci_halfwidth(results)
+    logger.info(f"{m:.4f} ±{hw:.4f}")
